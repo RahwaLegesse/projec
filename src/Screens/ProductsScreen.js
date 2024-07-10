de28f4fcf02b26d3077ee1a2060b44f953bc87e1
EsayasA <esayasaregawi29@gmail.com>
2024-07-10 21:07:56 +0400

    ggg

---
+++ src/Screens/ProductsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const ProductsScreen = () => {
+  return (
+    <div>
+      products
+    </div>
+  )
+}
+
+export default ProductsScreen
---
+++ src/data.js
@@ -0,0 +1,70 @@
+export default{
+    products:[
+        {
+            name:'Blue velo',
+            brand:'Velo',
+            catagory:'miniskirt',
+            Image:'images/images2.jpeg',
+            price:'60',
+            rating:'5 star review'
+
+    },
+    {
+        name:'Blue velo',
+        brand:'Velo',
+        catagory:'trouser',
+        Image:'images/images2.jpeg',
+        price:'80',
+        rating:'5 star review'
+
+    },
+    {
+    name:'Blue velo',
+    brand:'Velo',
+    catagory:'t-shirt',
+    Image:'images/images2.jpeg',
+    price:'90',
+    rating:'5 star review'
+    },
+    {
+        name:'Blue velo',
+        brand:'Velo',
+        catagory:'t-shirt',
+        Image:'images/images2.jpeg',
+        price:'90',
+        rating:'5 star review'
+        },
+        {
+            name:'Blue velo',
+            brand:'Velo',
+            catagory:'t-shirt',
+            Image:'images/images2.jpeg',
+            price:'90',
+            rating:'5 star review'
+            },
+            {
+                name:'Blue velo',
+                brand:'Velo',
+                catagory:'t-shirt',
+                Image:'images/images2.jpeg',
+                price:'90',
+                rating:'5 star review'
+                },
+                {
+                    name:'Blue velo',
+                    brand:'Velo',
+                    catagory:'t-shirt',
+                    Image:'images/images2.jpeg',
+                    price:'90',
+                    rating:'5 star review'
+                    },
+                    {
+                        name:'Blue velo',
+                        brand:'Velo',
+                        catagory:'t-shirt',
+                        Image:'images/images2.jpeg',
+                        price:'90',
+                        rating:'5 star review'
+                        },
+]
+}
